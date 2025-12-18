cd9fd97187f25a354daee576a2c03cfdf38a4f8b
Mayur Helkar <mayurhelkar@yahoo.com>
2025-12-18 10:48:21 +0530

    Add files via upload

---
+++ Programs/Swap_Values/swapValues_multipleLine_arithmeticAddSubtract.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    int a;          int b;
+
+    cout << "Input 2 values ::" << endl;
+    cin >> a;                    cin >> b;
+
+    cout << endl;
+    
+    cout << "Before Swapping ::" << endl;
+    cout << "a : " << a;         cout << "\t\tb : " << b;
+    
+    cout << endl << endl;
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    cout << "After Swapping ::" << endl;
+    cout << "a : " << a;          cout << "\t\tb : " << b;
+    
+    return 0;
+}
